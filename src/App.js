ec8a5469a37f47aebb5280b25f593cee6ea5b31b
Jo Blaszak <jblaszak@gmail.com>
2023-03-02 10:48:25 -0800

    Extracted input into own component and cleaned up defaults.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useMemo, useRef } from "react";
+import React, { useEffect, useRef } from "react";
+import Input from "./components/Input";
 
 import classes from "./App.module.css";
 
@@ -14,13 +15,6 @@ function App() {
 
   const breathRefs = [inhaleRef, holdRef, exhaleRef, hold2Ref];
   const breathNames = ["INHALE", "HOLD", "EXHALE", "HOLD"];
-
-  const setup = {
-    default: 2,
-    min: 0,
-    max: 15,
-    step: 0.1,
-  };
 
   useEffect(() => {
     let timer = null;
@@ -73,61 +67,12 @@ function App() {
   return (
     <>
       <div className={classes.breathContainer}>
-        <div className={classes.breath} data-breath="inhale" ref={breathRef}></div>
+        <div className={classes.breath} ref={breathRef}></div>
       </div>
       <div className={classes.controls} ref={controlsRef}>
-        <div className={classes.inputContainer}>
-          <label>Inhale</label>
-          <input
-            ref={inhaleRef}
-            id="inhale"
-            name="inhale"
-            type="number"
-            min={setup.min}
-            max={setup.max}
-            step={setup.step}
-            defaultValue={setup.default}
-          />
-        </div>
-        <div className={classes.inputContainer}>
-          <label>Hold</label>
-          <input
-            ref={holdRef}
-            id="hold"
-            name="hold"
-            type="number"
-            min={setup.min}
-            max={setup.max}
-            step={setup.step}
-            defaultValue={setup.default}
-          />
-        </div>
-        <div className={classes.inputContainer}>
-          <label>Exhale</label>
-          <input
-            ref={exhaleRef}
-            id="exhale"
-            name="exhale"
-            type="number"
-            min={setup.min}
-            max={setup.max}
-            step={setup.step}
-            defaultValue={setup.default}
-          />
-        </div>
-        <div className={classes.inputContainer}>
-          <label>Hold</label>
-          <input
-            ref={hold2Ref}
-            id="hold2"
-            name="hold"
-            type="number"
-            min={setup.min}
-            max={setup.max}
-            step={setup.step}
-            defaultValue={setup.default}
-          />
-        </div>
+        {breathRefs.map((breathRef, i) => {
+          return <Input key={`${breathNames[i] + i}`} ref={breathRef} label={breathNames[i]} />;
+        })}
       </div>
     </>
   );
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,20 +1,13 @@
+import { forwardRef } from "react";
 import classes from "./Input.module.css";
 
-const Input = ({ label, inputRef, defaultValue, value }) => {
+const Input = forwardRef(({ label, min = 0, max = 15, step = 0.1, defaultValue = 2 }, ref) => {
   return (
     <div className={classes.inputContainer}>
       <label>{label}</label>
-      <input
-        ref={inputRef}
-        type="number"
-        min="0"
-        max="15"
-        step="0.1"
-        defaultValue={defaultValue}
-        value={value}
-      />
+      <input ref={ref} type="number" min={min} max={max} step={step} defaultValue={defaultValue} />
     </div>
   );
-};
+});
 
 export default Input;
